9cc142fe1437c8a6488e8c4d54cf02e23c36c077
ujangaripin24 <ujangarifin06@gmail.com>
2025-11-03 13:49:33 +0700

    login and register form success

--- components/LoginScreen.tsx
+++ components/LoginScreen.tsx
@@ -1,51 +1,104 @@
 'use client'
 
+import { Button } from 'flowbite-react'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+import { FaUser, FaEye, FaGoogle, FaFacebook, FaCheck } from 'react-icons/fa'
+import { FaMessage } from 'react-icons/fa6'
 
 export default function LoginScreen() {
-    return (<div className=''>
-            <div className="w-screen h-screen grid grid-flow-col gap-1">
-                <div className="col-span-2 row-span-3 hidden md:block">
-                    <div className='flex justify-center items-center h-full'>
-                        <div className="text-3xl font-bold text-black">01</div>
-                    </div>
+
+  const handleShowPassword = () => {
+    console.log("Masuk");
+  }
+
+  return (
+    <section>
+      <div className='flex'>
+        <div className='w-screen hidden md:block p-6'>
+          <div className='flex items-center h-screen'>
+            <div>
+              <div>
+                <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+                  <Image width={0} height={0} className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
+                  Flowbite
+                </a>
+              </div>
+              <div className='text-4xl font-bold dark:text-white'>Selamat Datang Flowbite Chat</div>
+              <div>
+                <div>
+                  <div><FaCheck />
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, dolorum ipsam! Inventore dolorum facilis laboriosam!</p>
+                  </div>
                 </div>
-                <div className="col-span-2 row-span-3">
-                    <div className='flex justify-center items-center h-screen'>
-                        <form className="max-w-sm mx-auto">
-                            <div className="mb-5">
-                                <label htmlFor="email"
-                                       className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
-                                    email</label>
-                                <input type="email" id="email"
-                                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                       placeholder="name@flowbite.com" required/>
-                            </div>
-                            <div className="mb-5">
-                                      <label htmlFor="password"
-                                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
-                                    password</label>
-                                          <input type="password" id="password"
-                                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                       required/>
-                            </div>
-                                    <div className="flex items-start mb-5">
-                                <div className="flex items-center h-5">
-                                    <input id="remember" type="checkbox" value=""
-                                           className="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                                           required/>
-                                </div>
-                                <label htmlFor="remember"
-                                       className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember
-                                    me</label>
-                            </div>
-                            <button type="submit"
-                                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit
-                            </button>
-                        </form>
-                    </div>
+                <div>
+                  <div><FaCheck />
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non, perspiciatis.</p></div>
                 </div>
+                <div>
+                  <div><FaCheck />
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, quaerat.</p></div>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-    )
+        <div className='w-full'>
+          <div className='flex justify-center items-center h-screen'>
+            <div className="w-full max-w-md p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
+              <div>
+                <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+                  <Image width={0} height={0} className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
+                  Flowbite
+                </a>
+              </div>
+              <div className='mb-6'>
+                <div className='text-3xl font-bold dark:text-white'>Selamat Datang Kembali</div>
+                <div className='text-md dark:text-white'>Register sekarang <Link href="/login" className="text-blue-600 hover:underline dark:text-blue-500">Disini</Link></div>
+              </div>
+              <div className='flex max-w-md flex-col gap-4'>
+                <form className="">
+                  <div className='flex flex-col gap-4'>
+                    <div>
+                      <label htmlFor="email-address-icon" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Email</label>
+                      <div className="relative">
+                        <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                          <FaMessage className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" />
+                        </div>
+                        <input type="email" id="email-address-icon" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" />
+                      </div>
+                    </div>
+                    <div>
+                      <label htmlFor="password-address-icon" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Password</label>
+                      <div className="relative">
+                        <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                          <button onClick={() => handleShowPassword()}>
+                            <FaEye className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" />
+                          </button>
+                        </div>
+                        <input type="password" id="password-address-icon" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="******" />
+                      </div>
+                    </div>
+                    <div>
+                      <Button className='w-full' color={'light'} size="lg" type="submit">Submit</Button>
+                    </div>
+                  </div>
+                </form>
+                <hr />
+                <div className='flex flex-row gap-4'>
+                  <Button className='w-full' outline color={'blue'} size="lg" type="submit">
+                    <FaGoogle />
+                  </Button>
+                  <Button className='w-full' outline color={'blue'} size="lg" type="submit">
+                    <FaFacebook />
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
